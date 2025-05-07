a7fae5d6b766b04c2eef084f992ba7b7e33e007d
Yurgish <humenyuriipetrovych@gmail.com>
2025-05-08 00:57:40 +0300

    Add weather condition converter and update data binding
    Introduces `WeatherConditionToImageConverter` to dynamically display weather images based on conditions. Updates `MainPage.xaml` to use the converter for the weather image and modifies `MainPage.xaml.cs` to set the `BindingContext` for improved MVVM compliance, enhancing the overall data binding approach in the UI.

---
+++ Converters/WeatherConditionToImageConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherClient.Models;
+
+namespace WeatherClient.Converters
+{
+    public class WeatherConditionToImageConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is WeatherType weatherCondition)
+            {
+                return weatherCondition switch
+                {
+                    WeatherType.Sunny => ImageSource.FromFile("sunny.png"),
+                    WeatherType.Cloudy => ImageSource.FromFile("cloud.png"),
+                    _ => ImageSource.FromFile("question.png")
+                };
+            }
+
+            return ImageSource.FromFile("question.png");
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+            throw new NotImplementedException();
+    }
+}
